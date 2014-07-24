aab5d0e9d6a897ca1730d8b572286dd0e8755c65
Kevin Hilt <kevin@kevinhilt.com>
2014-07-23 20:03:23 -0400

    Added framework for options and preview panels

--- script.js
+++ script.js
@@ -11,4 +11,98 @@
 //and the plural logic in that language said 4=="many", 1=="one", and 3=="few"
 //would call ( {"GENDER": "female", "PLURAL1":"many", "PLURAL2": "one", "PLURAL3": "few"})
 
-document.getElementById("editor_content").innerHTML = "Test";
+var option_select_primary = document.getElementById("option_select_primary");
+var option_select_secondary = document.getElementById("option_select_secondary");
+var gender_options = ["male", "female", "other"];
+var plural_options = ["one", "other"];
+
+option_select_secondary.style.visibility = "hidden";
+add_button.style.visibility = "hidden";
+
+
+option_select_primary.onchange = function()
+{
+	var selected_value = this.value;
+	var add_button = document.getElementById("add_button");
+
+	clear_secondary_options(option_select_secondary);
+
+	option_select_secondary.style.visibility = "hidden";
+	add_button.style.visibility = "hidden";
+
+	if(selected_value === "empty")
+	{
+		return;
+	}
+
+	add_button.style.visibility = "visible";
+
+	if(selected_value === "NUMBER")
+	{
+		return;
+	}
+
+	option_select_secondary.style.visibility = "visible";
+
+	get_selection_options("option_select_secondary", selected_value);
+
+};
+
+var clear_secondary_options = function(element)
+{
+	while (element.firstChild)
+	{
+		element.removeChild(element.firstChild);
+	}
+};
+
+var get_selection_options = function(id, selected_value)
+{
+	var option_array = gender_options;
+	var loop_function = create_input_fields;
+	var element = document.getElementById(id);
+
+	if(selected_value == "PLURAL")
+	{
+		option_array = plural_options;
+	}
+
+	if(id != "option_select_secondary")
+	{
+		loop_function = populate_dropdown;
+	}
+
+	var count = option_array.length;
+
+	for(var i = 0; i < count; i++)
+	{
+		loop_function(element, option_array[i]);
+	}
+
+};
+
+var create_input_fields = function(element, placeholder)
+{
+	var current_input = document.createElement("input");
+	current_input.placeholder = placeholder;
+	element.appendChild(current_input);
+	var line_break = document.createElement("br");
+	element.appendChild(line_break);
+};
+
+var populate_dropdown = function(element, value)
+{
+	var current_option = document.createElement("option");
+	current_option.value = value;
+	current_option.innerHTML = value;
+	element.appendChild(current_option);
+};
+
+var update_preview_dropdowns = function()
+{
+	get_selection_options("preview_gender_select", "GENDER");
+	get_selection_options("preview_plural_select", "PLURAL");
+
+};
+
+update_preview_dropdowns();
