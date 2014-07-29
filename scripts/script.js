b2dadd110e4d43771aa3ed5005bec3bc8856f163
Kevin Hilt <kevin@kevinhilt.com>
2014-07-28 22:55:15 -0400

    Everything working, but still no messageformat preview yet

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,66 @@
+//TODO
+//Put included_locales list in a separate file.
+//Could be populated using filenames in the /locale directory?
+//Only problem with that is that you'd have "en" but not "English"
+//unless you add another key to en.json that's "name" or something
+
 var locale_data;
-var gender_options = ["male", "female", "other"];
-var plural_options = ["one", "other"];
+var included_locale_option_array = [];
+var included_locales = 
+{
+	ar: "Arabic",
+	en: "English"
+}
+
+if(!localStorage.getItem("current_locale"))
+{
+    localStorage.setItem("current_locale", "en");
+}
+
+var get_locale_data = function(selected_locale, passed_path) 
+{
+	var path;
+
+	if(passed_path)
+	{
+		path = passed_path;
+	}
+	else
+	{
+		path = "locales/" + selected_locale + ".json";
+	}
+
+    var xobj = new XMLHttpRequest();
+    var returned_data;
+
+
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, false);
+
+    xobj.onreadystatechange = function() 
+    {
+        handle_ready_state(xobj);
+    }
+
+    xobj.send(null);
+
+}
+
+var handle_ready_state = function(xobj) 
+{
+    if (xobj.readyState == 4 && xobj.status == "200") 
+    {
+        locale_data = JSON.parse(xobj.responseText);
+    } 
+    else if (xobj.readyState == 4 && xobj.status == "404") 
+    {
+    	console.log("Couldn't load " + localStorage.getItem("current_locale") + ".json");
+    	localStorage.setItem("current_locale", "en");
+    	locale_data = { "gender": ["male", "female", "other"], "plural": ["one", "other"] }; 
+    }
+}
+	
+get_locale_data(localStorage.getItem("current_locale"));
 
 document.getElementById("option_select_primary_1").onchange = function()
 {
@@ -73,15 +133,32 @@ var clear_secondary_options = function(element)
 	}
 }
 
+var initialize_included_locale_option_array = function()
+{
+	var included_locale_keys = Object.keys(included_locales);
+	var count = included_locale_keys.length;
+
+	for(var i = 0; i < count; i++)
+	{
+		//Creates something like [ ["ar", "Arabic"], ["en", "English"] ]
+		included_locale_option_array[i] = 
+		[ included_locale_keys[i], included_locales[included_locale_keys[i]] ];
+	}
+}();
+
 var get_selection_options = function(id, selected_value, panel_number)
 {
-	var option_array = gender_options;
+	var option_array = locale_data.gender;
 	var loop_function = create_input_field;
 	var element = document.getElementById(id);
 
 	if(selected_value == "plural")
 	{
-		option_array = plural_options;
+		option_array = locale_data.plural;
+	}
+	else if(selected_value == "included_locales")
+	{
+		option_array = included_locale_option_array;
 	}
 
 	if(id.slice(0,-2) != "option_select_secondary")
@@ -95,7 +172,6 @@ var get_selection_options = function(id, selected_value, panel_number)
 	{
 		loop_function(element, option_array[i], selected_value, panel_number);
 	}
-
 }
 
 var create_input_field = function(element, option_name, selected_value, panel_number)
@@ -182,18 +258,28 @@ var add_new_option_close = function(panel_number, previous_panel_number)
 	document.getElementById("options_panel_" + panel_number).style.display = "none";
 }
 
-var populate_dropdown = function(element, option_name)
+var populate_dropdown = function(element, option_value)
 {
+	var option_name;
+
+	if(typeof option_value == "object")
+	{
+		option_name = option_value[1];
+		option_value = option_value[0];
+	}
+
 	var current_option = document.createElement("option");
-	current_option.value = option_name;
-	current_option.innerHTML = option_name;
+	current_option.value = option_value;
+	current_option.innerHTML = option_name || option_value;
 	element.appendChild(current_option);
 }
 
-var update_preview_dropdowns = function()
+var update_dropdowns = function()
 {
-	get_selection_options("preview_gender_select", "GENDER");
-	get_selection_options("preview_plural_select", "PLURAL");
+	get_selection_options("preview_gender_select", "gender");
+	get_selection_options("preview_plural_select", "plural");
+	get_selection_options("editor_locale_select", "included_locales");
+	document.getElementById("editor_locale_select").value = localStorage.getItem("current_locale");
 
 }();
 
@@ -221,6 +307,22 @@ var preview_button_onclick = function()
 	preview_text.innerHTML += "<br/>TODO: Include messageformat.js and compile with the above as input."
 }
 
+var locale_dropdown_onchange = function()
+{
+	var locale_dropdown = document.getElementById("editor_locale_select");
+	var response = confirm("Because different languages have different plural rules, changing the language will clear all the fields on the page. Are you sure you want to change the language?")
+
+	if(response == true)
+	{
+    	localStorage.setItem("current_locale", locale_dropdown.value);
+    	location.reload();
+	}
+	else
+	{
+    	locale_dropdown.value = localStorage.getItem("current_locale");
+	}
+}
+
 var reset_preview = function()
 {
 	var preview_text = document.getElementById("preview_content_text");
@@ -232,40 +334,4 @@ var reset_preview = function()
 reset_preview();
 //to be sloppy.
 
-var get_locale_data = function(selected_locale) 
-{
 
-    var path = "locales/" + selected_locale + ".json";
-    var xobj = new XMLHttpRequest();
-    var returned_data;
-
-
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', path, false);
-
-    xobj.onreadystatechange = function() 
-    {
-        handle_ready_state(xobj);
-    }
-
-    xobj.send(null);
-
-}
-
-var handle_ready_state = function(xobj) 
-{
-    if (xobj.readyState == 4 && xobj.status == "200") 
-    {
-        locale_data = JSON.parse(xobj.responseText);
-
-    } 
-    else if (xobj.readyState == 4 && xobj.status == "404") 
-    {
-        locale_data = get_locale_data("en");
-
-    }
-}
-
-get_locale_data("en");
-
-console.log(locale_data.gender);
